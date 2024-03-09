6812f299abf44da15e2d68565e513fbcbcff755c
klau55 <kipatok@bk.ru>
2024-03-09 15:39:26 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ function App() {
   return (
     <>
       <list>
-        <li>HELLO</li>
-        <li>WORLD</li>
-        <li>WELCOME</li>
-        <li>TO</li>
-        <li>HELL</li>
+        <a>HELLO WORLD</a>
+        <li>IDEAS:</li>
+        <li>bad-habits tracker</li>
+        <li>“Personal gym buddy” finder</li>
+        <li>good habits tracker ^_^</li>
         <button onClick={() => setCount((count) => count + 1)}>click if you dare</button>
         <p>{count}</p>
       </list>
